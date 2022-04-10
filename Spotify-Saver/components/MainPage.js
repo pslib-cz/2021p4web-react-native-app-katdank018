27d9fa2e7ff72d9e323fbf4b875c48ec7b3876d3
Kateřina Daňková <katerina.dankova@pslib.cz>
2022-04-10 21:06:34 +0200

    search našeptávání

--- Spotify-Saver/components/MainPage.js
+++ Spotify-Saver/components/MainPage.js
@@ -1,26 +1,66 @@
-import { useRef } from "react";
+import axios from "axios";
+import { useRef, useState, useEffect } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import { api } from "../configuration/spotifyConfig";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const MainPage = () => {
-    const searchText = useRef();
+  const [searchText, setSearchText] = useState("");
+  const [accessToken, setAccessToken] = useState(null);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const GetAccessToken = async () => {
+      try {
+        setAccessToken(await AsyncStorage.getItem("access_token"));
+      } catch (e) {
+        console.log("Error", e);
+      }
+    };
+
+    GetAccessToken();
+  }, [accessToken]);
+
+  useEffect(() => {
+    // console.log(accessToken);
+    // console.log(searchText);
+    // console.log(api + "search?q=" + searchText + "&type=artist&limit=3");
+    if (searchText !== "") {
+      axios({
+        method: "get",
+        url: api + "search?q=" + searchText + "&type=artist&limit=3",
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      }).then(function (response) {
+        setResults(response.data.artists.items.map((a) => a.name));
+      });
+      console.log(results);
+    }
+  }, [searchText]);
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.search} placeholder="Hledat..." ref={searchText}></TextInput>
+      <TextInput
+        style={styles.search}
+        placeholder="Hledat umělce..."
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+      ></TextInput>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 50
-    },
-    search: {
-        borderWidth: 1,
-        margin: 20,
-        height: 50,
-        padding: 10
-    }
+  container: {
+    marginTop: 50,
+  },
+  search: {
+    borderWidth: 1,
+    margin: 20,
+    height: 50,
+    padding: 10,
+  },
 });
 
 export default MainPage;
--- Spotify-Saver/configuration/spotifyConfig.js
+++ Spotify-Saver/configuration/spotifyConfig.js
@@ -1,3 +1,3 @@
 export const client_id = '68ad02cff07e46828183146f75f9c77d'; // Your client id
 export const client_secret = '2b57ae5d12044a8ab3315ceebeea71b4'; // Your secret
-// var scopes = \'user-read-private user-read-email\'
+export const api = 'https://api.spotify.com/v1/';
